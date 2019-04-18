1557f7ec40787c6994a048b8abe1c367631b551f
ricbon35 <ricbon35@gmail.com>
2019-04-18 16:14:11 +0200

    v0

---
+++ src/main/java/fr/diginamic/approche_objet2/App.java
@@ -0,0 +1,13 @@
+package fr.diginamic.approche_objet2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
